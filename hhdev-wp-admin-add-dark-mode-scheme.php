338476a0877bd4a9a36db0df70cbbcff9a550936
herbert: hoekstra <werk@haha.nl>
2022-10-06 23:23:58 +0200

    Update hhdev-wp-admin-add-dark-mode-scheme.php
    some css url changes

--- hhdev-wp-admin-add-dark-mode-scheme.php
+++ hhdev-wp-admin-add-dark-mode-scheme.php
@@ -5,31 +5,26 @@
  * Description: Add dark mode scheme for the WordPress dashboard user profile. Based on the Dark Mode for WP Dashboard CSS file.
  * Author: herbert hoekstra - haha.nl - hrbrt.dev
  * Author URI: https://haha.nl
- * Version: 1.0.0
+ * Version: 1.0.1
  */
 
  if ( ! defined( 'ABSPATH' ) ) {
      die();
  }
 
- define('HHDEV_DARK_MODE_DASHBOARD_PLUGIN_PATH', plugin_dir_url(__FILE__));
-
-
 // add the darkmode color scheme
 function hhdev_additional_admin_color_schemes() {
 
-  //Get the theme directory
-  $theme_dir = get_stylesheet_directory_uri();
-  //Ocean
+  //Darkmode
   wp_admin_css_color( 'darkmode', __( 'Dark Mode' ),
-    $theme_dir . '/admin-colors/dark-mode-dashboard.css',
+    esc_url( plugins_url('css/dark-mode-dashboard.css',__FILE__)),
     array( '#aa9d88', '#9ebaa0', '#738e96', '#f2fcff' )
   );
 }
 add_action('admin_init', 'hhdev_additional_admin_color_schemes');
 
-// set default color scheme to darkmode
-
+// force set default color scheme to darkmode
+// SET OFF
 function hhdev_update_user_option_admin_color( $color_scheme ) {
     $color_scheme = 'darkmode';
 
